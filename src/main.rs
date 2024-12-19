4badfcd5aafcfb4036585b162b532a10370723cc
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-12-19 04:18:11 +0200

    *

--- src/main.rs
+++ src/main.rs
@@ -11,19 +11,19 @@ fn main() {
     }
   }";
   println!("Error:");
-  println!("{}", dunh::high_err_ctx(38, 64, &code, 2));
-  println!("");
+  println!("{}", dunh::high_err_ctx(38, 64, code, 2));
+  println!();
 
   println!("Warning:");
-  println!("{}", dunh::high_warn(38, 64, &code));
-  println!("");
+  println!("{}", dunh::high_warn(38, 64, code));
+  println!();
 
   println!("Custom color:");
-  println!("{}", dunh::high(38, 64, &code, "\x1b[4m\x1b[32m"));
-  println!("");
+  println!("{}", dunh::high(38, 64, code, "\x1b[4m\x1b[32m"));
+  println!();
 
   let code = "(Foo x) = 7[0 ]\n";
   println!("Warning:");
-  println!("{}", dunh::high_err(16, 17, &code));
-  println!("");
+  println!("{}", dunh::high_err(16, 17, code));
+  println!();
 }
